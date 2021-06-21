9405a5d5f36d3a7cb9456bce207a0f11c3fa8f2b
Hendra Manudinata <hendralize@gmail.com>
2021-06-21 14:46:52 +0000

    Add extras for m30sdd-user 11 RP1A.200720.012 M307FXXU4CUD2 release-keys
    Signed-off-by: Hendra Manudinata <hendralize@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## m30sdd-user 11 RP1A.200720.012 M307FXXU4CUD2 release-keys
+- Manufacturer: samsung
+- Platform: universal9611
+- Codename: m30s
+- Brand: samsung
+- Flavor: m30sdd-user
+- Release Version: 11
+- Id: RP1A.200720.012
+- Incremental: M307FXXU4CUD2
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: undefined
+- Fingerprint: samsung/m30sdd/m30s:11/RP1A.200720.012/M307FXXU4CUD2:user/release-keys
+- OTA version: 
+- Branch: m30sdd-user-11-RP1A.200720.012-M307FXXU4CUD2-release-keys
+- Repo: samsung_m30s_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null >> system/system/priv-app/SecSettings/SecSettings.apk
+rm -f system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null
+cat system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/priv-app/Velvet/Velvet.apk
+rm -f system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null >> system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk
+rm -f system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null
+cat system/system/priv-app/AvatarEmojiSticker_Palette/AvatarEmojiSticker_Palette.apk.* 2>/dev/null >> system/system/priv-app/AvatarEmojiSticker_Palette/AvatarEmojiSticker_Palette.apk
+rm -f system/system/priv-app/AvatarEmojiSticker_Palette/AvatarEmojiSticker_Palette.apk.* 2>/dev/null
+cat system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null >> system/system/priv-app/AREmoji/AREmoji.apk
+rm -f system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null
+cat system/system/app/Maps/Maps.apk.* 2>/dev/null >> system/system/app/Maps/Maps.apk
+rm -f system/system/app/Maps/Maps.apk.* 2>/dev/null
+cat system/system/app/YouTube/YouTube.apk.* 2>/dev/null >> system/system/app/YouTube/YouTube.apk
+rm -f system/system/app/YouTube/YouTube.apk.* 2>/dev/null
+cat system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null >> system/system/app/HoneyBoard/HoneyBoard.apk
+rm -f system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null
+cat system/system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
